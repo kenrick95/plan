a62ce06dd409302aad5a78e7549ceb8b754fa828
Edwin Candinegara <edocsss@gmail.com>
2014-11-29 23:39:59 +0800

    Adding simple testing website

---
+++ Tester/engine.js
@@ -0,0 +1,7 @@
+$(document).ready(function($) {
+	$("#course_form#submit").click( function () {
+		/*
+			Check whether the input is valid (using regex)
+		*/
+	});
+});
---
+++ Tester/readme.txt
@@ -0,0 +1,4 @@
+NOT DONE YET!!
+Just a simple form to test the correctness of the scheduler
+
+Will do scheduler first after this, then the AJAX thingy
