b0e51ed699df13305a0ef04035b948cd84f3f82e
Edwin Candinegara <edwin.candinegara@gmail.com>
2018-04-25 22:51:57 +0800

    Use a date object to get the current year and semester

--- back_end/config.php
+++ back_end/config.php
@@ -2,6 +2,6 @@
 ini_set('memory_limit', '512M');
 error_reporting(0);
 
-$year = isset($_REQUEST['year']) ? intval($_REQUEST['year']) : 2017;
-$semester = isset($_REQUEST['semester']) ? intval($_REQUEST['semester']) : 2;
+$year = isset($_REQUEST['year']) ? intval($_REQUEST['year']) : date("Y");
+$semester = isset($_REQUEST['semester']) ? intval($_REQUEST['semester']) : date("n") <= 6 ? 1 : 2;
 date_default_timezone_set("UTC");
