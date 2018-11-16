85fe5a8bdbad59d09f8500acd9cb22fd18de1bec
Kenrick <kenrick95@gmail.com>
2018-11-16 15:20:04 +0800

    Update data for 2018 sem 2

--- back_end/config.php
+++ back_end/config.php
@@ -3,5 +3,5 @@ ini_set('memory_limit', '512M');
 error_reporting(0);
 
 $year = isset($_REQUEST['year']) ? intval($_REQUEST['year']) : 2018;
-$semester = isset($_REQUEST['semester']) ? intval($_REQUEST['semester']) : 1;
+$semester = isset($_REQUEST['semester']) ? intval($_REQUEST['semester']) : 2;
 date_default_timezone_set("UTC");
