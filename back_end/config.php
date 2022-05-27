db5d2f1d0528708a7ef500eb8f0e4161b2fc3ad3
Kenrick <kenrick95@users.noreply.github.com>
2022-05-27 12:19:22 +0000

    Update config

--- back_end/config.php
+++ back_end/config.php
@@ -2,6 +2,6 @@
 ini_set('memory_limit', '512M');
 error_reporting(0);
 
-$year = isset($_REQUEST['year']) && intval($_REQUEST['year']) > 0 ? intval($_REQUEST['year']) : 2021;
-$semester = isset($_REQUEST['semester']) && intval($_REQUEST['semester']) > 0 ? intval($_REQUEST['semester']) : 2;
+$year = isset($_REQUEST['year']) && intval($_REQUEST['year']) > 0 ? intval($_REQUEST['year']) : 2022;
+$semester = isset($_REQUEST['semester']) && intval($_REQUEST['semester']) > 0 ? intval($_REQUEST['semester']) : 1;
 date_default_timezone_set("UTC");
